7681ededb35557598cb18b6026bf952a9082b11d
huuthang28cb <huuthang28.work@gmail.com>
2022-08-18 21:19:54 +0700

    first commit

---
+++ public/admins/product/index/list.js
@@ -0,0 +1,38 @@
+function actionDelete(event){
+    event.preventDefault();
+    let urlRequest = $(this).data('url');
+    let that = $(this); // chính là cái button click vào á
+    Swal.fire({
+        title: 'Bạn có chắc chắn muốn xóa không?',
+        text: "Bạn sẽ không thể hoàn nguyên điều này!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đúng, tôi muốn xóa!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: 'GET',
+                url: urlRequest,
+                success: function (data){
+                    if(data.code==200){
+                        that.parent().parent().remove();
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                },
+                error: function (){
+
+                }
+            });
+        }
+    })
+}
+
+$(function(){
+   $(document).on('click', '.action_delete', actionDelete);
+});
---
+++ routes/web.php
@@ -0,0 +1,162 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/admin', "AdminController@loginAdmin");
+Route::post('/admin', "AdminController@postloginAdmin");
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::prefix('admin')->group(function () {
+
+    //Category
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [
+            'as' => 'categories.index',
+            'uses' => 'CategoryController@index',
+        ]);
+        Route::get('/create', [
+            'as' => 'categories.create',
+            'uses' => 'CategoryController@create',
+        ]);
+        Route::post('/store', [
+            'as' => 'categories.store',
+            'uses' => 'CategoryController@store',
+        ]);
+        Route::get('/edit/{id}', [
+            'as' => 'categories.edit',
+            'uses' => 'CategoryController@edit',
+        ]);
+        Route::post('/update/{id}', [
+            'as' => 'categories.update',
+            'uses' => 'CategoryController@update',
+        ]);
+        Route::get('/delete/{id}', [
+            'as' => 'categories.delete',
+            'uses' => 'CategoryController@delete',
+        ]);
+    });
+
+    //Menus
+    Route::prefix('menus')->group(function () {
+        Route::get('/', [
+            'as' => 'menus.index',
+            'uses' => 'MenuController@index',
+        ]);
+        Route::get('/create', [
+            'as' => 'menus.create',
+            'uses' => 'MenuController@create',
+        ]);
+        Route::post('/store', [
+            'as' => 'menus.store',
+            'uses' => 'MenuController@store',
+        ]);
+        Route::get('/edit/{id}', [
+            'as' => 'menus.edit',
+            'uses' => 'MenuController@edit',
+        ]);
+        Route::post('/update/{id}', [
+            'as' => 'menus.update',
+            'uses' => 'MenuController@update',
+        ]);
+        Route::get('/delete/{id}', [
+            'as' => 'menus.delete',
+            'uses' => 'MenuController@delete',
+        ]);
+    });
+
+    //Product
+    Route::prefix('product')->group(function () {
+        Route::get('/', [
+            'as' => 'product.index',
+            'uses' => 'AdminProductController@index',
+        ]);
+        Route::get('/create', [
+            'as' => 'product.create',
+            'uses' => 'AdminProductController@create',
+        ]);
+        Route::post('/store', [
+            'as' => 'product.store',
+            'uses' => 'AdminProductController@store',
+        ]);
+        Route::get('/edit/{id}', [
+            'as' => 'product.edit',
+            'uses' => 'AdminProductController@edit',
+        ]);
+        Route::post('/update/{id}', [
+            'as' => 'product.update',
+            'uses' => 'AdminProductController@update',
+        ]);
+        Route::get('/delete/{id}', [
+            'as' => 'product.delete',
+            'uses' => 'AdminProductController@delete',
+        ]);
+    });
+
+    //Slider
+    Route::prefix('slider')->group(function () {
+        Route::get('/', [
+            'as' => 'slider.index',
+            'uses' => 'SliderAdminController@index',
+        ]);
+        Route::get('/create', [
+            'as' => 'slider.create',
+            'uses' => 'SliderAdminController@create',
+        ]);
+        Route::post('/store', [
+            'as' => 'slider.store',
+            'uses' => 'SliderAdminController@store',
+        ]);
+        Route::get('/edit/{id}', [
+            'as' => 'slider.edit',
+            'uses' => 'SliderAdminController@edit',
+        ]);
+        Route::post('/update/{id}', [
+            'as' => 'slider.update',
+            'uses' => 'SliderAdminController@update',
+        ]);
+        Route::get('/delete/{id}', [
+            'as' => 'slider.delete',
+            'uses' => 'SliderAdminController@delete',
+        ]);
+    });
+
+    //Setting
+    Route::prefix('settings')->group(function () {
+        Route::get('/', [
+            'as' => 'settings.index',
+            'uses' => 'AdminSettingController@index',
+        ]);
+        Route::get('/create', [
+            'as' => 'settings.create',
+            'uses' => 'AdminSettingController@create',
+        ]);
+        Route::post('/store', [
+            'as' => 'settings.store',
+            'uses' => 'AdminSettingController@store',
+        ]);
+        Route::get('/edit/{id}', [
+            'as' => 'settings.edit',
+            'uses' => 'AdminSettingController@edit',
+        ]);
+        Route::post('/update/{id}', [
+            'as' => 'settings.update',
+            'uses' => 'AdminSettingController@update',
+        ]);
+        Route::get('/delete/{id}', [
+            'as' => 'settings.delete',
+            'uses' => 'AdminSettingController@delete',
+        ]);
+    });
+});
